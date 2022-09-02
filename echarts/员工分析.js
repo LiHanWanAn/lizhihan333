fb5ef3e435d0da4804821da341eb9dd572211fb6
lizhihan <1773214058@qq.com>
2022-09-02 18:43:28 +0800

    添加代码

--- echarts/员工分析.js
+++ echarts/员工分析.js
@@ -13,12 +13,13 @@ function changeTime() {
 var newtime = window.setInterval(changeTime, 1000);
 
 lzh(1);
+var y1 = window.setTimeout(Lzh1, 1000);
 lzh(2);
+var y2 = window.setTimeout(Lzh2, 1000);
 lzh(3);
 lzh(4);
 lzh(5);
 lzh(6);
-var ns = window.setTimeout(Lzh, 1000);
 
 function lzh(a) {
     var xhr = new XMLHttpRequest();
@@ -37,6 +38,24 @@ function lzh(a) {
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.send(JSON.stringify(obj));
 }
-function Lzh() {
-    
+function Lzh1() {
+    var y1 = JSON.parse(sessionStorage['y1']);
+    var arr1=document.getElementsByClassName("ZJ_P2");
+    var arr2=document.getElementsByClassName("L_ZJ_p2");
+    arr1[0].innerHTML=y1.今日销售额
+    arr1[1].innerHTML=y1.今日客单价
+    arr1[2].innerHTML=y1.今日交易数
+    arr1[3].innerHTML=(y1.今日销售额/y1.今天上班员工数).toFixed(2);
+    arr2[0].innerHTML=y1.本周销售额
+    arr2[1].innerHTML=y1.本月销售额
+    arr2[2].innerHTML=y1.本周客单价
+    arr2[3].innerHTML=y1.本月客单价
+    arr2[4].innerHTML=y1.本周交易数
+    arr2[5].innerHTML=y1.本月交易数
+    arr2[6].innerHTML=(y1.本周销售额/y1.本周上班员工数).toFixed(2);
+    arr2[7].innerHTML=(y1.本月销售额/y1.本月上班员工数).toFixed(2);
+}
+function Lzh2() {
+    var y2 = JSON.parse(sessionStorage['y2']);
+    console.log(y2)
 }
