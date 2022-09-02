1bab17265f20d0425a91be72a8d0b3c66228cf60
lizhihan <1773214058@qq.com>
2022-09-02 16:55:29 +0800

    JS

--- echarts/员工分析.js
+++ echarts/员工分析.js
@@ -5,15 +5,21 @@ function changeTime() {
     var currentDate = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
     var hh = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
     var mm = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-    var ss = date.getSeconds() <10 ? "0" + date.getSeconds() : date.getSeconds();
-    var time = [fullyear,month,currentDate,hh,mm,ss]
+    var ss = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+    var time = [fullyear, month, currentDate, hh, mm, ss]
     var d = document.getElementsByClassName("time")[0];
-    d.innerHTML=time[0]+'年'+time[1]+'月'+time[2]+'日'+time[3]+'时'+time[4]+'分'+time[5]+'秒'
+    d.innerHTML = time[0] + '年' + time[1] + '月' + time[2] + '日' + time[3] + '时' + time[4] + '分' + time[5] + '秒'
 }
-var newtime = window.setInterval(changeTime,1000);
+var newtime = window.setInterval(changeTime, 1000);
 
-var lzh1= lzh(1);
-console.log(lzh1)
+lzh(1);
+lzh(2);
+lzh(3);
+lzh(4);
+lzh(5);
+lzh(6);
+var ns = window.setTimeout(Lzh, 1000);
+
 function lzh(a) {
     var xhr = new XMLHttpRequest();
     var obj = {
@@ -22,12 +28,15 @@ function lzh(a) {
     xhr.onreadystatechange = function () {
         if (xhr.status == 200 && xhr.readyState == 4) {
             var res = xhr.response;
-            res = JSON.parse(res);
-            console.log(res)
-            return res;
+            sessionStorage['y'+a] = res;
+            // res = JSON.parse(res);
         }
+        return res;
     }
-    xhr.open("post", "http://127.0.0.1:8080/shapan/analysis_charts/astanly/", true);
+    xhr.open("post", "http://127.0.0.1:8080/shapan/analysis_charts/astanly/", false);
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.send(JSON.stringify(obj));
+}
+function Lzh() {
+    
 }
